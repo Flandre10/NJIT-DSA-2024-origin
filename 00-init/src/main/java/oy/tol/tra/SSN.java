92eb8af76419e8c85e29b9547f659e20dc2a1007
Flandre10 <126231286+Flandre10@users.noreply.github.com>
2024-03-11 14:59:20 +0800

    Add files via upload

--- 00-init/src/main/java/oy/tol/tra/SSN.java
+++ 00-init/src/main/java/oy/tol/tra/SSN.java
@@ -100,7 +100,9 @@ public class SSN {
                String personNumberString = ssn.substring(PERSON_CODE_INDEX_START,
                      PERSON_CODE_INDEX_START + PERSON_CODE_LENGTH);
                // Next checking the three digit number after the century separator.
+               if (personNumberString.matches("[0-9]+")){
                Integer personNumber = Integer.parseInt(personNumberString);
+               
                if (personNumber > 0) {
                   // If it was a positive integer, then calculate the checksum.
                   StringBuilder builder = new StringBuilder();
@@ -117,6 +119,7 @@ public class SSN {
                         // Checksum was correct for SSNs valid for testing systems.
                         result = Result.VALID_TEST_SSN;
                      }
+                  }
                   }
                }
             }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -22,8 +22,7 @@ public class Grades {
    /**
     * The method to reverse the internal Java int array.
     */
-   public void reverse() {
-      /* TODO:
+    /* TODO:
        1. Edit the test data files to see if the reverse() really works or not.
        2. Execute the IntArrayTests to see that some of them fail.
        3. Study the code below and try to find what is the issue.
@@ -31,8 +30,10 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
+   public void reverse() {
+      
       int i = 0;
-      while (i <= grades.length/2) {
+      while (i < grades.length/2) {
          int temp = grades[i];
          grades[i] = grades[grades.length-i-1];
          grades[grades.length-i-1] = temp;
@@ -52,16 +53,24 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int i = grades.length-1;
-      while (i > 0) {
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
+         boolean sorted = false; // 是否已排序
+         int n = grades.length;
+         
+         while (!sorted) {
+             sorted = true; // 假设数组已排序
+             
+             for (int i = 1; i < n; i++) {
+                 if (grades[i] < grades[i - 1]) {
+                     int tmp = grades[i];
+                     grades[i] = grades[i - 1];
+                     grades[i - 1] = tmp;
+                     sorted = false; // 数组尚未排序
+                 }
+             }
+             
+             n--; // 减少未排序部分的范围
          }
-         i--;
-      }
-   }
+     }
 
    /**
     * Returns the plain Java int [] array for investigation.
